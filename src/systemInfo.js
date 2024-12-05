5634e4abc755f9258083db0eab63ce67d1164a37
Jalen King <jkin2520@students.vsu.edu>
2024-12-05 08:56:02 -0500

    Added documentation for all functions

--- src/systemInfo.js
+++ src/systemInfo.js
@@ -1,8 +1,15 @@
 const os = require('os');
-
+/**
+ * Returns the number of CPUs on the system.
+ * @returns {number} Number of CPU cores.
+ */
 function getCPUCount() {
     return os.cpus().length;
 }
+/**
+ * Retrieves system architecture and total memory.
+ * @returns {{arch: string, totalMemory: number}} An object with system architecture and memory.
+ */
 function getSystemInfo() {
     const os = require('os');
     return {
@@ -10,6 +17,11 @@ function getSystemInfo() {
         totalMemory: os.totalmem(),
     };
 }
+/**
+ * Checks if the system has at least the specified memory in megabytes.
+ * @param {number} mb - Memory to check in megabytes.
+ * @returns {boolean} True if sufficient memory is available, otherwise false.
+ */
 function hasEnoughMemory(mb) {
     const os = require('os');
     const availableMemory = os.totalmem() / (1024 * 1024); // Convert to MB

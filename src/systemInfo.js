d23250be91465116905d0a433e22136af7bff29a
Jalen King <jkin2520@students.vsu.edu>
2024-12-05 08:51:53 -0500

    Implement hasEnoughMemory function

--- src/systemInfo.js
+++ src/systemInfo.js
@@ -10,5 +10,10 @@ function getSystemInfo() {
         totalMemory: os.totalmem(),
     };
 }
+function hasEnoughMemory(mb) {
+    const os = require('os');
+    const availableMemory = os.totalmem() / (1024 * 1024); // Convert to MB
+    return availableMemory >= mb;
+}
 
-module.exports = { getCPUCount, getSystemInfo };
+module.exports = { getCPUCount, getSystemInfo, hasEnoughMemory };
